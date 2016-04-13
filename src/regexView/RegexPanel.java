1689986f2df8f74a59af9b1d41732dfb3fe770e4
htha9587 <harrison.thacker69@gmail.com>
2016-04-13 12:03:30 -0600

    Version 0.3
    Added all RegexPanel data members.

--- src/regexView/RegexPanel.java
+++ src/regexView/RegexPanel.java
@@ -1,5 +1,30 @@
 package regexView;
 
-public class RegexPanel {
+import regexController.RegexController;
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
 
+/**
+ * Version 0.3
+ * 4-13-16
+ * @author htha9587
+ *
+ */
+
+
+public class RegexPanel extends JPanel
+{
+	private RegexController baseController;
+	private JButton enterButton;
+	private JLabel firstLabel;
+	private JLabel secondLabel;
+	private JLabel thirdLabel;
+	private JLabel fourthLabel;
+	private SpringLayout baseLayout;
+	private JTextField firstField;
+	private JTextField secondField;
+	private JTextField thirdField;
+	private JTextField fourthField;
 }
